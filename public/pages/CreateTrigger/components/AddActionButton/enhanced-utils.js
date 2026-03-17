9aa1d0683ecbea0b3c84881f7105b92795dc1231
Antonio David Gutiérrez <antonio.gutierrez@wazuh.com>
2026-03-17 17:47:54 +0100

    fix(active-response): error on monitor edition

--- public/pages/CreateTrigger/components/AddActionButton/enhanced-utils.js
+++ public/pages/CreateTrigger/components/AddActionButton/enhanced-utils.js
@@ -7,6 +7,7 @@ import _ from 'lodash';
 import { getInitialActionValues as getInitialNotificationsValues } from './utils';
 import { FORMIK_INITIAL_ACTION_VALUES } from '../../utils/constants';
 import { getDigitId, getUniqueName } from '../../../../utils/helpers';
+import { NOTIFY_OPTIONS_VALUES } from '../Action/actions/Message';
 
 const getInitialActiveResponseValues = ({ monitorType, flyoutMode, actions }) => {
   const initialActionValues = _.cloneDeep(FORMIK_INITIAL_ACTION_VALUES);
@@ -16,6 +17,18 @@ const getInitialActiveResponseValues = ({ monitorType, flyoutMode, actions }) =>
 
   const id = getDigitId();
   initialActionValues.id = `activeResponse${id}`;
+
+  // Add the action_action_execution_scope by alert
+  // RESEARCH: this property is not part of the initial action values, and could be added by some forms
+  // that are not present for the active response
+  initialActionValues.action_execution_policy = {
+    action_execution_scope: {
+      [NOTIFY_OPTIONS_VALUES.PER_ALERT]: {
+        'actionable_alerts': []
+      }
+    }
+  }
+
 
   // Add name based on previous name;
   if (flyoutMode) {
