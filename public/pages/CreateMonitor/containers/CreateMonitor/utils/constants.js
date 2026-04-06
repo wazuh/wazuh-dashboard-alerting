152376d6cffadf210476069a59d3132a0d4a0a6a
Rodrigo Lopez <rodrigo.lopez@wazuh.com>
2026-04-06 19:53:48 +0000

    chore: add comment

--- public/pages/CreateMonitor/containers/CreateMonitor/utils/constants.js
+++ public/pages/CreateMonitor/containers/CreateMonitor/utils/constants.js
@@ -106,7 +106,7 @@ export const DEFAULT_DOCUMENT_LEVEL_QUERY = JSON.stringify(
     queries: [
       {
         name: 'QUERY_NAME',
-        query: '*',
+        query: '*', // Wazuh: use DQL instead
         tags: ['TAG_TEXT'],
       },
     ],
